7c7c8669c54a2fcd8743817703a3892495eafa9b
Ric Taglietti <rictaglietti@gmail.com>
2022-03-01 19:30:08 -0500

    fetch and display posts from server

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,37 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import Search from './components/Search';
+
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    async function getPosts() {
+      const response = await fetch('http://localhost:3001/posts');
+      const data = await response.json();
+      setPosts(data);
+    }
+    getPosts();
+  }, []);
+
+  const filteredPosts = posts.filter((post) =>
+    post.title.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <div>
-      <h1>Hello World</h1>
+      <Navbar />
+      <section className="container">
+        <Search search={search} setSearch={setSearch} />
+        {filteredPosts.map((post) => (
+          <div key={post.id} className="post">
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+          </div>
+        ))}
+      </section>
     </div>
   );
 }
